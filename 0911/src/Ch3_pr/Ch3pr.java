7de89ee15db7b60e488ae0a3f4da7cb309028836
KimJinPyo1 <jyfct5646@gmail.com>
2025-09-12 20:32:13 +0900

    0912

--- 0911/src/Ch3_pr/Ch3pr.java
+++ 0911/src/Ch3_pr/Ch3pr.java
@@ -3,7 +3,23 @@ package Ch3_pr;
 public class Ch3pr {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = -100;
+		int result1 = +a;
+		System.out.println(result1);
+		
+		int x = 10;
+		x++;
+		
+		System.out.println(x);
+		
+		boolean play = true;
+		
+		if(play)
+			System.out.println("true");
+		
+		int score = 85;
+		char grade = (score>90) ? 'A' : ((score>80) ? 'B' : 'C');
+		System.out.println(grade);
 
 	}
 
