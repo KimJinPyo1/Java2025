6ad2c9541398a3cf6b7513c425d38b20186d5991
KimJinPyo1 <jyfct5646@gmail.com>
2025-09-25 22:23:06 +0900

    0925

---
+++ 0925/src/Ch7pr3/SmartPhone.java
@@ -0,0 +1,21 @@
+package Ch7pr3;
+
+public class SmartPhone extends Phone{
+	
+	public SmartPhone(String owner)
+	{
+		super(owner);
+	}
+	
+	public void internetSearch()
+	{
+		System.out.println("인터넷 검색을 합니다");
+	}
+	
+	@Override
+	public void sound()
+	{
+		System.out.println("dog");
+	}
+
+}
