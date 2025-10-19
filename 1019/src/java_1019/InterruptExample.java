48391ce54983fd25747aa59254a3661e79cabef4
KimJinPyo1 <jyfct5646@gmail.com>
2025-10-19 19:57:14 +0900

    1019

---
+++ 1019/src/java_1019/InterruptExample.java
@@ -0,0 +1,15 @@
+package java_1019;
+
+public class InterruptExample {
+
+	public static void main(String[] args) {
+		Thread thread = new PrintThread2();
+		thread.start();
+		
+		try { Thread.sleep(1000); } catch(InterruptedException e) {}
+		
+		thread.interrupt();
+
+	}
+
+}
