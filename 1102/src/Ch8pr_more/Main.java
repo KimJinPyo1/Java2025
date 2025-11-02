725ff3f22238e9a6a766dec39a11bb9f36978574
KimJinPyo1 <jyfct5646@gmail.com>
2025-11-02 16:54:02 +0900

    1102

---
+++ 1102/src/Ch8pr_more/Main.java
@@ -0,0 +1,14 @@
+package Ch8pr_more;
+
+public class Main {
+
+	public static void main(String[] args) {
+				
+				SmartTelevision tv = new SmartTelevision();
+				
+				RemoteControl rc = tv;
+				Searchable searchable = tv;
+
+	}
+
+}
---
+++ 1102/src/Ch8pr_more/Searchable.java
@@ -0,0 +1,6 @@
+package Ch8pr_more;
+
+public interface Searchable {
+	void search(String url);
+
+}
