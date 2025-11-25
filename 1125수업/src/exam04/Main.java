69d5b6b22ecf8f532b8207c79a23874d6f337db0
KimJinPyo1 <jyfct5646@gmail.com>
2025-11-25 10:43:42 +0900

    1125

---
+++ 1125수업/src/exam04/Main.java
@@ -0,0 +1,14 @@
+package exam04;
+
+import java.util.HashMap;
+
+public class Main {
+
+	public static void main(String[] args) {
+		HashMap<String, String> hashMap = 
+				new HashMap<String, String>();
+		
+
+	}
+
+}
---
+++ 1125수업/src/exam06/Main.java
@@ -0,0 +1,18 @@
+package exam06;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		Key key1 = new Key(123);
+//		Object obj = new Object();
+//		
+//		System.out.println(key1);
+//		System.out.println(key1.toString());
+//		System.out.println(obj);
+		
+		SmartPhone phone1 = new SmartPhone("Samsung", "Android");
+		System.out.println(phone1);
+
+	}
+
+}
